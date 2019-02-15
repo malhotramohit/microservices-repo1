0b79fc809591c09e46e9ef15828d840108281ee2
mohitmalhotra <mohitmalhotra@mohits-air>
2019-02-16 00:19:58 +0530

    Second Commit

--- currency-calculation/src/main/java/com/gs/ilp/rest/currencycalculation/currencycalculation/CurrencyCalculationProxy.java
+++ currency-calculation/src/main/java/com/gs/ilp/rest/currencycalculation/currencycalculation/CurrencyCalculationProxy.java
@@ -5,11 +5,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service" )
+@FeignClient(name = "zuul-server" )
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyCalculationProxy {
 
-	@GetMapping("/currency-exchange/{from}/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/{from}/{to}")
 	public CurrencyExchangeResponse getExchangeValue(@PathVariable(name = "from") String from,
 			@PathVariable(name = "to") String to);
 }
